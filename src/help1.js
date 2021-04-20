e0e1dbe074aa61d8e4ffeaa11c398a8e8f107892
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-20 12:35:36 -0500

    Update help1.js

--- src/help1.js
+++ src/help1.js
@@ -1,9 +1,9 @@
 const help1 = (prefix) => {
 	return `
-┃MENU EM ATUALIZAÇÃO ⚠️
-┣QUALQUER COISA
-┣CHAME O DONO
-┣wa.me/+5522996215481`
+┃MENU ATUALIZADO ⚠️
+┣Cualquier cosa
+┣Llame al dueño
+┣wa.me/+5521970019765
 }
 exports.help1 = help1
 
