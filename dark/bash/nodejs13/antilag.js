0e9da3548329d1381770dbf172266f26b7322605
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-20 12:11:22 -0500

    Update antilag.js

--- dark/bash/nodejs13/antilag.js
+++ dark/bash/nodejs13/antilag.js
@@ -1,13 +1,13 @@
 const antilag = (prefix) => { 
 	return `
 	
-	*Soluçao! Este recurso ainda está em desenvolvimento, aguarde a última atualização da versão Dark Bot*
+	*¡Solución! Esta función aún está en desarrollo, espere la última actualización de la versión de Dark Bot*
 	
-[❗] *As últimas atualizações do WhatsApp Bot estão apenas no canal do Youtube : Darkkk*
+[❗] *Las últimas actualizaciones de WhatsApp Bot están solo en el canal de YouTube: Darkkk*
 
-⧿⧽ *Não se esqueça de se inscrever*
-⧿⧽ *Ative os sinos*
-⧿⧽ *Atenciosamente Dark*
+⧿⧽ *No olvides suscribirte*
+⧿⧽ *Ative la campana*
+⧿⧽ *Atentamente Dark*
 
 `
 }
