87d6922a7a5b2837da0f4e845083668afe98460e
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-20 11:35:06 -0500

    Update antiban.js

--- dark/bash/nodejs13/antiban.js
+++ dark/bash/nodejs13/antiban.js
@@ -1,13 +1,13 @@
 const antiban = (prefix) => { 
 	return `
 	
-	*Soluçao! Este recurso ainda está em desenvolvimento, aguarde a última atualização da versão Dark Bot*
+	*¡Solución! Esta función aún está en desarrollo, espere la última actualización de la versión Dark Bot*
 	
-[❗] *As últimas atualizações do WhatsApp Bot estão apenas no canal do Youtube : Darkkk*
+[❗] [❗] * Las últimas actualizaciones de WhatsApp Bot están solo en el canal de YouTube*
 
-⧿⧽ *Não se esqueça de se inscrever*
-⧿⧽ *Ative os sinos*
-⧿⧽ *Atenciosamente Dark*
+⧿⧽ *No olvides suscribirte*
+⧿⧽ *Activar la campanita*
+⧿⧽ *Atentamente DarkZE*
 
 `
 }
